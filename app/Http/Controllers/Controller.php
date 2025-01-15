634fa37b4f1fe609ef772465c87315da5f704174
1wikii <ahmaddwikyzerrodixxon@gmail.com>
2025-01-15 23:40:43 +0700

    feat : tambah breeze untuk auth

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,29 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Routing\Controller as BaseController;
-
-class AuthController extends BaseController
+abstract class Controller
 {
-    public function register(Request $request)
-    {
-        $request->validate([
-            'fullname' => 'required|string|max:255',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:8',
-        ]);
-
-        User::create([
-            'fullname' => $request->fullname,
-            'email' => $request->email,
-            'gender' => $request->gender,
-            'phone' => $request->phone,
-            'age' => $request->age,
-        ]);
-
-        return redirect('/login')->with('success', 'Pendaftaran berhasil!');
-    }
+    //
 }
