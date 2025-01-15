d3b550827779997885a36b34fdbcb9588938e06e
ahmadrizki007 <ahmad.121140105@student.itera.ac.id>
2025-01-15 22:03:23 +0700

    Controller.php

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,11 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('register'); 
 })->name('register');
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::post('/register', [AuthController::class, 'register']);
