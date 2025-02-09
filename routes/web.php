6f51e8dba2667342409e99a5f8cce90db72de4b6
1wikii <ahmaddwikyzerrodixxon@gmail.com>
2025-02-09 09:47:54 +0700

    fix : bug profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProfilePrevController;
 use Illuminate\Support\Facades\Route;
 
 // INI CUMAN BUAT DEV AJA
@@ -18,9 +19,9 @@ Route::get('/', function () {
 
 
 Route::middleware('auth')->group(function () {
-    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile/prev', [ProfilePrevController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/update', [ProfilePrevController::class, 'update'])->name('profile.update');
+    Route::delete('/profile/destroy', [ProfilePrevController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 
