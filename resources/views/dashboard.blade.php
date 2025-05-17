6acb4ae74d5eee97c0d3dd5ffc30f2bfebc3df03
1wikii <ahmaddwikyzerrodixxon@gmail.com>
2025-05-17 16:18:10 +0700

    fix: update dashboard title

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.layout')
 
 @section('title')
-    <title>Profile</title>
+    <title>Dashboard</title>
 @endsection
 
 @section('scripts')
@@ -37,22 +37,22 @@
             <div class="flex gap-6 lg:flex-row flex-col">
                 <!-- Card: Dompet Kamu (Balance) -->
                 <div x-data="{ mode: null,
-                        balance: {{ Auth::user()->balance }}, 
-                        amount: '',
-                        minimumWithdraw: 10000,
-                        get numericAmount() {
-                            return Number(this.amount.replaceAll('.', '').replaceAll(',', '')) || 0;
-                        },
-                        get error() {
-                            return this.numericAmount > this.balance;
-                        },
+                            balance: {{ Auth::user()->balance }}, 
+                            amount: '',
+                            minimumWithdraw: 10000,
+                            get numericAmount() {
+                                return Number(this.amount.replaceAll('.', '').replaceAll(',', '')) || 0;
+                            },
+                            get error() {
+                                return this.numericAmount > this.balance;
+                            },
 
-                        get errorMinimum(){
-                            if(!this.error && this.amount !== ''){
-                            return this.numericAmount < this.minimumWithdraw;
+                            get errorMinimum(){
+                                if(!this.error && this.amount !== ''){
+                                return this.numericAmount < this.minimumWithdraw;
+                                }
                             }
-                        }
-                        }" class="sm:p-6 h-full p-3 bg-white rounded-lg shadow-lg flex flex-col">
+                            }" class="sm:p-6 h-full p-3 bg-white rounded-lg shadow-lg flex flex-col">
                     <div class="sm:text-2xl text-lg text-gray-600 mb-4">Dompet Kamu</div>
                     <div class="sm:text-lg font-inter text-primary-gray">Saldo Tersedia</div>
                     <div class="sm:text-4xl text-3xl font-semibold mt-4 mb-4">
